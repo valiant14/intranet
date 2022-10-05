505cbd5363169ee63ea349696ec432d511ca370a
valiant.ocampo14 <valiant.ocampo14@gmail.com>
2022-10-05 22:20:26 +0300

    sdfasfd

--- src/View/StaffEssentials/index.js
+++ src/View/StaffEssentials/index.js
@@ -1,21 +1,42 @@
-import React from "react";
-// import {db} from '../../firebase/firebase'
-// import { Button } from "react-bootstrap";
-// import auth from '../../firebase/firebase'
+import React,{useState} from "react";
+import {Link } from "react-router-dom";
+import './index.scss'
 
 const StaffEssentials = ({data}) => {
+    const [contextData, setContextData] = useState([])
+
 
     return(
         <React.Fragment>
         <hr></hr>
-        {data.map((item,index) => {
-            console.log(item.Content)
-            return (
-                <div key={index}>
-                    {item.Content}
+                <div className="menu">
+                    <ul>
+                        {data.map((item,index) => {
+                            return (
+                                <li key={index}>
+                                    <a onClick={() => setContextData(item.Type)}>{item.Content}</a>
+                                </li>
+                            );
+                            })}
+                    </ul>
                 </div>
-            )
-        })}
+                <div className="content-menu">
+                    <div className="file-explorer">
+                        <ul>
+                            <li><a>File</a></li>
+                            <li><a>View</a></li>
+                            <li><a>Sort</a></li>
+                            <li><a>Share</a></li>
+                        </ul>
+                            <div className="form">
+                                <i className="fa fa-search"></i>
+                                <input type="text" className="form-control form-input" placeholder="Search anything..."></input>
+                                <span className="left-pan"><i className="fa fa-microphone"></i></span>
+                            </div>
+                    </div>
+
+                    <h1>{contextData}</h1>
+                </div>
         </React.Fragment>
     )
 
